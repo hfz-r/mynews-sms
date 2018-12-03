43afb1d16c4724d698dd738cb83002f9046a44b7
NORAZIRA <NORAZIRA@b-excelle.com>
2018-12-03 14:26:16 +0800

    Account Controller

---
+++ StockManagementSystem/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace StockManagementSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        //
+        // GET: /Account/Login
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        //
+        // GET: /Account/Register
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/LogOff
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LogOff()
+        {
+            return RedirectToAction(nameof(AccountController.Login), "Account");
+        }
+    }
+}
