d5cb99117a2c9286249934244b110c39f0b1f4eb
Yenn Yenn <yennyenn@b-excelle.com>
2018-12-14 18:01:56 +0800

    Amend skin color
    Add device view page (Without function)

---
+++ StockManagementSystem/Controllers/DeviceController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StockManagementSystem.Controllers
+{
+    public class DeviceController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("Device");
+        }
+
+        [HttpGet]
+        public IActionResult RegisterDevice()
+        {
+            return View("RegisterDevice");
+        }
+    }
+}
