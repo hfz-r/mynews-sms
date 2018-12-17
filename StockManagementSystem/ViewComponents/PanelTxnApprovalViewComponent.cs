a9498c6dc04956754ca21ab4026a0f805f69ce55
Yenn Yenn <yennyenn@b-excelle.com>
2018-12-17 16:09:12 +0800

    Add screens for
    1. Stock Order Limit
    2. Approval Setting
    3. Shelf Location

---
+++ StockManagementSystem/ViewComponents/PanelTxnApprovalViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockManagementSystem.ViewComponents
+{
+    [ViewComponent(Name = "PanelTxnApprovalComponent")]
+    public class PanelTxnApprovalViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
