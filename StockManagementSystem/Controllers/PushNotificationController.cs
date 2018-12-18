b311b8894eb4c5fea4c62c037b0e897db2913886
NORAZIRA <NORAZIRA@b-excelle.com>
2018-12-18 11:51:20 +0800

    Add screen for push notification.

---
+++ StockManagementSystem/Controllers/PushNotificationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StockManagementSystem.Controllers
+{
+    public class PushNotificationController : Controller
+    {
+        public IActionResult AddNotification()
+        {
+            return View("AddNotification");
+        }
+
+        public IActionResult Notification()
+        {
+            return View("Notification");
+        }
+    }
+}
