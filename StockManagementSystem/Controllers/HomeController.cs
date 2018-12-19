9a866b9c403c5e472840248635e186151158bc88
Yenn Yenn <yennyenn@b-excelle.com>
2018-12-19 12:14:45 +0800

    Amend login and register page

--- StockManagementSystem/Controllers/HomeController.cs
+++ StockManagementSystem/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace StockManagementSystem.Controllers
 {
-    //[Authorize]
+    [Authorize]
     public class HomeController : Controller
     {
         public IActionResult Index()
