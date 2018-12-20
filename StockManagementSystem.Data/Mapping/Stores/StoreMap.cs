9ac6c98c8518a8028014d7459fe6fba6f6d4c3f9
Yenn Yenn <yennyenn@b-excelle.com>
2018-12-20 10:28:55 +0800

    Change status datatype at Device
    Add View module for device management(Manage device)

---
+++ StockManagementSystem.Data/Mapping/Stores/StoreMap.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StockManagementSystem.Core.Domain.Stores;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockManagementSystem.Data.Mapping.Stores
+{
+    public partial class StoreMap : EntityTypeConfiguration<Store>
+    {
+        public override void Configure(EntityTypeBuilder<Store> builder)
+        {
+            //Import from Master table
+            builder.ToTable("Store");
+            builder.HasKey(store => store.Id);
+            
+            base.Configure(builder);
+        }
+    }
+}
--- StockManagementSystem/Controllers/DeviceController.cs
+++ StockManagementSystem/Controllers/DeviceController.cs
@@ -1,18 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc;
+using StockManagementSystem.Core;
+using StockManagementSystem.Core.Domain.Devices;
+using StockManagementSystem.Models.Devices;
+using System;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace StockManagementSystem.Controllers
 {
     public class DeviceController : Controller
     {
+        private bool _disposed;
+
+        private readonly IRepository<Device> _deviceRepository;
+
+        #region Constructor
+
+        public DeviceController(IRepository<Device> deviceRepository)
+        {
+            this._deviceRepository = deviceRepository;
+        }
+
+        #endregion
+
+        #region Destructor
+
+        ~DeviceController()
+        {
+            Dispose(false);
+        }
+
+        #endregion
+
+        #region IDisposable 
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+
         [HttpGet]
         public IActionResult Index()
         {
-            return View("Device");
+            DeviceViewModel model = new DeviceViewModel();
+            model.Device = _deviceRepository.Table.Select(x => new Device
+            {
+                Id = x.Id,
+                TokenId = x.TokenId,
+                SerialNo = x.SerialNo,
+                ModelNo = x.ModelNo,
+                Longitude = x.Longitude,
+                Latitude = x.Latitude,
+                StoreId = "TEST", //TODO
+                Status = x.Status == "1" ? "Active" : "Inactive",
+                CreatedBy = x.CreatedBy,
+                CreatedOn = Convert.ToDateTime(x.CreatedOn.ToString()),
+                ModifiedBy = x.ModifiedBy,
+                ModifiedOn = Convert.ToDateTime(x.ModifiedOn.ToString())
+            }).ToList();
+
+            return View("Device", model);
         }
+
+
 
         [HttpGet]
         public IActionResult RegisterDevice()
