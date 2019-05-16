5923010ae6c7d87cab10f1f6c8cdc0563b5c666b
Yenn Yenn <yennyenn@b-excelle.com>
2019-05-16 18:02:13 +0800

    Add new table - Warehouse delivery schedule master

---
+++ StockManagementSystem.Data/Mapping/Master/WarehouseDeliveryScheduleMasterMap.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StockManagementSystem.Core.Domain.Master;
+
+namespace StockManagementSystem.Data.Mapping.Master
+{
+    public partial class WarehouseDeliveryScheduleMasterMap : EntityTypeConfiguration<WarehouseDeliveryScheduleMaster>
+    {
+        public override void Configure(EntityTypeBuilder<WarehouseDeliveryScheduleMaster> builder)
+        {
+            builder.ToTable(nameof(WarehouseDeliveryScheduleMaster));
+
+            base.Configure(builder);
+        }
+    }
+}
